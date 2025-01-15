25271719efa7037e6bcf3f5650e3cd99204062ae
Francis Kaela Bonhayag <bonhayagkaela@gmail.com>
2025-01-15 17:37:20 +0800

    form8 not done

--- complaint-hub/src/components/Form8.jsx
+++ complaint-hub/src/components/Form8.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import "../main.css";
-import Header from "./Header";
+import Header from "./Header"; //
+import PdfForm8 from "./pdfForm8";
 
 const Form8 = () => {
+  const [formData, setFormData] = useState({
+    form8KpCaseNumber: "",
+    form8Kay: "",
+    form8Sumbong: "",
+    form8Date: "",
+    form8Day: "",
+    form8Month: "",
+    form8Year: "",
+    form8DayUtos: "",
+    form8MonthUtos: "",
+    form8YearUtos: "",
+    form8DayNow: "",
+    form8MonthNow: "",
+    form8YearNow: "",
+    form8DayBiso: "",
+    form8MonthBiso: "",
+    form8YearBiso: "",
+    form8Time: "",
+    
+  });
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  }
+
   return (
     <div className="form8-page">
       <Header showButton={false} />
@@ -14,7 +45,7 @@ const Form8 = () => {
           <form className="form8">
             <div className="form8-kp-input">
               <label className="form8-kpcase">KP Case Number:</label>
-              <input type="text" id="form8-kpnum" placeholder=" " />
+              <input type="text" id="form8KpCaseNumber" onChange={handleInputChange} />
             </div>
             <div className="form8-pormularyoblg">
               <label className="form8-pormularyo">Pormularyo ng KP Blg. 8</label>
@@ -33,9 +64,9 @@ const Form8 = () => {
               </label>
             </div>
             <div className="form8-date">
-              <input type="text" id="form8-date" placeholder=" " />
+              <input type="text" id="form8Date" onChange={handleInputChange} />
               <label>, 20</label>
-              <input type="text" id="form8-year" placeholder=" " />
+              <input type="text" id="form8Year" onChange={handleInputChange} />
             </div>
             <div className="form8-group">
               <label className="form8-centered-label">
@@ -46,9 +77,9 @@ const Form8 = () => {
             <div className="form8-may-sumbong">
               <div className="form8-kay-input">
                 <label>KAY: </label>
-                <input type="text" id="form8-kay" placeholder=" " />
+                <input type="text" id="form8Kay" onChange={handleInputChange} />
               </div>
-              <input type="text" id="form8-sumbong" placeholder=" " /> <br/>
+              <input type="text" id="form8Sumbong" onChange={handleInputChange} /> <br/>
               <label className="form8-text-sumbong"> (Mga) May Sumbong </label>
             </div>
             <br /> <br />
@@ -56,25 +87,25 @@ const Form8 = () => {
               <label className="form8-indent">
                 Ikaw ay inuutusan na sa akin sa ika-
               </label>
-              <input type="text" id="form8-day" placeholder=" " />
+              <input type="text" id="form8DayUtos" onChange={handleInputChange} />
               <label>araw ng</label>
-              <input type="text" id="form8-month" placeholder=" " />
+              <input type="text" id="form8MonthUtos" onChange={handleInputChange} />
               <label>, 20</label>
-              <input type="text" id="form8-year" placeholder=" " />
+              <input type="text" id="form8YearUtos" onChange={handleInputChange} />
             </div>
             <div className="form8-form-input">
               <label>sa ganap na ika-</label>
-              <input type="text" id="form8-time" placeholder=" " />
+              <input type="text" id="form8Time" onChange={handleInputChange} />
               <label>ng umaga/hapon, para sa pagdinig ng inyong sumbong.</label>
             </div>
             <br />
             <div className="form8-form-input">
               <label className="form8-indent">Ngayong ika-</label>
-              <input type="text" id="form8-day" placeholder=" " />
+              <input type="text" id="form8DayNow" onChange={handleInputChange} />
               <label>araw ng</label>
-              <input type="text" id="form8-month" placeholder=" " />
+              <input type="text" id="form8MonthNow" onChange={handleInputChange} />
               <label>, 20</label>
-              <input type="text" id="form8-year" placeholder=" " />
+              <input type="text" id="form8YearNow" onChange={handleInputChange} />
               <label>.</label>
             </div>
             <br />
@@ -88,11 +119,11 @@ const Form8 = () => {
             <br /> <br />
             <div className="form8-form-input">
               <label className="form8-indent">Pinaaabisuhan ngayong ika-</label>
-              <input type="text" id="form8-day" placeholder=" " />
+              <input type="text" id="form8DayBiso" onChange={handleInputChange} />
               <label> ng</label>
-              <input type="text" id="form8-month" placeholder=" " />
+              <input type="text" id="form8MonthBiso" onChange={handleInputChange} />
               <label>, 20</label>
-              <input type="text" id="form8-year" placeholder=" " />
+              <input type="text" id="form8YearBiso" onChange={handleInputChange} />
               <label>.</label>
             </div>
             <br /> <br />
@@ -120,6 +151,12 @@ const Form8 = () => {
             Proceed to Form 9{" "}
           </button>
         </div>
+        <PDFDownloadLink
+          document={<pdfForm8 data={formData} />}
+          fileName="Form8.pdf"
+        >
+        {({ loading }) => (loading ? "Generating PDF..." : <button>Download PDF</button>)}
+        </PDFDownloadLink>
       </div>
     </div>
   );
